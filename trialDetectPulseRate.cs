f855cb7b9ae6f64fd9983190938465e2a9d857b3
orlacasey <orla.casey@ymail.com>
2018-02-16 23:37:18 +0100

    Add files via upload
    I am in search of wisdom, I am failing at this attempt so far but I want to persevere. The code comes from openube and it wouldn't run without bugs and I genuinely cant figure it out. I would like to expand the sphere so I can later change that gameobject to a different modelled shape. Can you wise people help? 
    
    this is the original https://github.com/openube/Unity-Pulse-Sensor but it's not working, I'm am new to this but I have been following the instructions so I'm confused as to why it's not working. 
    Cheers

---
+++ trialDetectPulseRate.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Threading;
+using System.IO.Ports;
+using UnityEngine.UI;
+
+public class DetectPulseRate : MonoBehaviour 
+{
+	public  static SerialPort serialPort;
+	public  static string streamRead;
+	public static int BPM;
+
+	public GameObject sphere;
+	public GameObject heartText;
+
+	void Start() 
+	{
+		OpenConnection();
+	}
+
+	public void OpenConnection() 
+	{
+		serialPort = new SerialPort("/dev/cu.usbmodem1421", 115200, Parity.None, 8, StopBits.One);
+
+		if(serialPort != null) 
+		{
+			if(!serialPort.IsOpen) 
+			{
+				serialPort.Close();
+				Debug.Log ("Chiudo la porta, perché è già aperta");
+			}
+			else 
+			{
+				serialPort.Open();
+				serialPort.ReadTimeout = 100;
+				Debug.Log("Porta aperta!");
+			}
+		}
+		else 
+		{
+			if(serialPort.IsOpen)
+				Debug.Log("the port is open");
+		}
+	}
+
+	void FixedUpdate()
+	{
+		if (serialPort.IsOpen && serialPort.BytesToRead > 0) {
+			streamRead = serialPort.ReadLine ();
+			BPM = int.Parse (streamRead);
+
+			sphere.transform.localScale = new Vector3 (1, 1, 1);
+		}
+
+		Vector3 sphereScale = new Vector3 (BPM / 100.0f, BPM / 100.0f, BPM / 100.0f);
+
+		heartText.GetComponent<Text> ().text = "BPM " + BPM;
+		sphere.transform.localScale = Vector3.Lerp (sphere.transform.localScale, sphereScale, 0.1f);
+
+		serialPort.BaseStream.Flush ();
+
+	}
+
+
+	void OnApplicationQuit() 
+	{
+		// The port is closed
+
+		if (serialPort != null)
+			serialPort.Close();
+	}
+}
